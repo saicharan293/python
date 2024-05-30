91078adaadd0e9718c9188819ca7b3e82cacc150
saicharan293 <2493saicharan@gmail.com>
2024-05-30 16:31:18 +0530

    variables storage and swap of numbers are added

---
+++ jenny_s_lecture/swap_num.py
@@ -0,0 +1,14 @@
+a = int(input("first number is "))
+b = int(input("second number is "))
+print("a value is", a)
+print("b value is", b)
+a = a + b
+b = a - b
+a = a - b
+print('swapped-----')
+print("a value is", a)
+print("b value is", b)
+#second method using third variable
+#c=a
+#a=b
+#b=c
---
+++ jenny_s_lecture/variables.py
@@ -0,0 +1,3 @@
+name = input("what's your name? ")
+print(name)
+print('name length is',len(name))
