3a456a46e46767efb8bcd2500cb9f977f728f258
saicharan293 <2493saicharan@gmail.com>
2024-06-10 12:40:53 +0530

    function polymorphism is added

---
+++ jenny_s_lectures/oops_in_python/polymorphism.py
@@ -0,0 +1,17 @@
+# polymorphism: same name but can be executed on different items or terms
+
+
+# function polymorphism: len( )
+
+x='Hello world'
+print('word length',len(x))
+mytuple = ("apple", "banana", "cherry")
+print(f'tuple length is {len(mytuple)}')
+dict_dob = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+print('dictionary length is',len(dict_dob))
+
