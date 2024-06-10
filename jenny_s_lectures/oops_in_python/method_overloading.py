17257e9a05f0e389502acd8a1d29e8498aca5837
saicharan293 <2493saicharan@gmail.com>
2024-06-10 18:12:27 +0530

    using dispatch decorator for exact overloading in python

--- jenny_s_lectures/oops_in_python/method_overloading.py
+++ jenny_s_lectures/oops_in_python/method_overloading.py
@@ -2,22 +2,32 @@
 #method overloading is not available in python,
 #  so it would take the latest method definition
 # possible ways: use of default arguments,variable length argument(*args)
-
+from multipledispatch import dispatch
 class Demo:
 
     # using default args
     # def add(self,a,b,c=0):
     #     return f'sum is {a+b+c}'
 
+
     #using variable length args (*args) and loop if req
-    def add(self,*args):
-        total=0
-        for _ in args:
-            total+= _
-        return f'sum = {total}'
+    # def add(self,*args):
+    #     total=0
+    #     for _ in args:
+    #         total+= _
+    #     return f'first, sum = {total}'
     
+    #using dispatch decorator
+    @dispatch(int, int)
+    def add(self, a, b):
+      return f'second,sum = {a+b}'
+    @dispatch(int, int, int)
+    def add(self, a, b, c):
+        # x = a+b+c
+        return f'third,sum = {a+b+c}'
+        
         
 d=Demo()
 # print(d.add(2,3))
-print(d.add(2,3,4))
-print(d.add(1,2,3,4,5,5))
+print(d.add(2,3,))
+print(d.add(1,2,3))
