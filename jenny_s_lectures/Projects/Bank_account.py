30a0bcfd23d13d992e12c1ff51e3c1bf3384db1e
saicharan293 <2493saicharan@gmail.com>
2024-06-11 16:55:10 +0530

    Bank account program is added

--- jenny_s_lectures/Projects/Bank_account.py
+++ jenny_s_lectures/Projects/Bank_account.py
@@ -3,8 +3,21 @@ class BankAccount:
         self.name=name
         self.balance=balance
 
+    def deposit(self,deposit):
+        self.balance+=deposit
+        print(f"Deposited {deposit}rs into {self.name}'s account")
+    
+    def withdraw(self,amt):
+        if amt>self.balance:
+            print(f"{self.name} cannot withdraw {amt}rs, since balance is {self.balance}rs")
+        else:
+            self.balance-=amt
+            print(f"{self.name} withdrew {amt}rs, remaining balance: {self.balance}rs")
     def __str__(self):
-        return f"Account holder: {self.name}\tBalance: {self.balance}"
+        return f"Account holder: {self.name}\nBalance: {self.balance}rs"
     
 ramesh=BankAccount('Ramesh',20000)
-print(ramesh)
+# print(ramesh)
+ramesh.deposit(5000)
+ramesh.withdraw(3000)
+# print(ramesh)
