f3e55b20d37c650f4453392401a555f423c78416
saicharan293 <2493saicharan@gmail.com>
2024-06-13 12:01:02 +0530

    turtle library started

---
+++ jenny_s_lectures/Projects/turtle_project/turtle1.py
@@ -0,0 +1,59 @@
+from turtle import *
+
+# getscreen()
+color('blue')
+# fillcolor('green')
+width(3)
+#triangle shape
+# for step in range(3):
+#     forward(100)
+#     left(120)
+# home()
+begin_fill()
+
+#circular form of shape:
+# while True:
+#     forward(230)
+#     left(170)
+#     if abs(pos()) < 1:
+#         break
+
+
+# spiral structure
+# for steps in range(100):
+#     for c in ('blue', 'red', 'green'):
+#         color(c)
+#         forward(steps)
+#         right(30)
+
+# rectangle
+# for steps in range(1):
+#     for c in ('red','yellow'):
+#         color(c)
+#         forward(150)
+#         right(90)
+#         forward(100)
+#         right(90)
+
+# pentagon
+lt=['red','yellow','blue','orange','green']
+
+for i in range(3):
+    fillcolor(lt[i])
+    begin_fill()
+    print(lt[i])
+    for _ in range(len(lt)):
+        color(lt[_])
+        forward(100)
+        left(72)
+    end_fill()
+    left(120)
+right(5)  
+forward(150)
+right(80)
+forward(200)
+shape('turtle')
+# hideturtle()
+exitonclick()
+# done()
+# mainloop()
